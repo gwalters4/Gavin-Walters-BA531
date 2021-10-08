baf2f126753c51c128872f0292604d3a250345c7
gwalters4 <gavin.g.walters@gmail.com>
2021-10-08 10:18:34 -0500

    Main V2
    final commit

--- JavaScriptApplication/JavaScriptApp.js
+++ JavaScriptApplication/JavaScriptApp.js
@@ -1,46 +1,29 @@
+const { get } = require('http');
 const recipe = require('./recipemodule.js');
-const ingredient = require('./recipemodule');
+const ingredient = require('./recipemodule.js');
 const recipeEndPoint = '/api/recipes';
 const ingredientsEndpoint = '/api/ingredients';
 
-let recipes = [];     //recipes array
-let ing = [];         //ingredients array
-function getRecipes(){      //new function getRecipes
-    return new Promise(function(resolve, rejects){
+function getRecipes() {      //new function getRecipes
+    return new Promise(function (resolve, rejects) {
         recipe.getData(recipeEndPoint)          //calls getData and passes the recipeEndpoint
-        .then(function(response){               //after results are returned passes to response
-            response.forEach(item => {          //itterates through each item adding it to recipe
-                item.ingredients.forEach(i => {
-                    ing.push(i);
+            .then(function (response) {               //after results are returned passes to response
+                response.forEach(item => {          //itterates through each item adding it to recipe
+                    console.log(item.name);
+                    console.log(item.ingredients);
                 });
-                let r = new recipe.recipe(item.name, ing);
-                recipes.push(r);
-                console.log(item.name)
-            });
-            //console.log(recipes);
-            //console.log(ing);
-        }).catch(function(error){               //catches error
-            console.log(error);                 //prints error
-        });
+            })
+    }).catch(function (error) {               //catches error
+        console.log(error);                 //prints error
     });
-    
 };
 
-let ingredients = [];
-function getIngredients(){
-    return new Promise(function(resolve, rejects){
-        ingredient.getData(ingredientsEndpoint)
-        .then(function(response){
-            response.forEach(item => {
-                ingredients.push(ingredient.ingredient(item.name, item.maxPPG))
-                console.log(item.name)
-            });
-        }).catch(function(error){
-            console.log(error);
-        });
-    });
-} 
 
-getRecipes();                                   //calls getRecipes
-//console.log(recipes);                           //Print recipes Array
-getIngredients();                               //calls getIngredients
+
+
+
+
+
+getRecipes();
+//recipe.getData(recipeEndPoint)
+recipe.calcMaxPPG();
